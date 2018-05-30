0c6e7af50153aa81391918ad20343b9004d27b14
Alessandro Gario <alessandrogario@users.noreply.github.com>
2018-05-30 07:04:41 +0200

    Santa: Add write support to the santa_rules table (#15)
    * santa: Add INSERT and DELETE support to the santa_rules table
    
    Other changes include
    1. Some code refactoring
    2. The santa_events table now includes all EXEC actions (you can
       filter by ALLOW/DENY and UNKNOWN - issued to unknown binaries
       when Santa is NOT running in lockdown mode)
    
    * Use shasum + rule_type as primary key
    
    Two rules with same shasum but different type fields (one set to
    certificate and the other one to binary) can exist at the same
    time inside the Santa database. This was not previously supported
    and was causing errors during insertion.
    
    * Fix a rule duplication error during INSERT handling

--- santa/src/santa.cpp
+++ santa/src/santa.cpp
@@ -20,6 +20,8 @@
 #include <sstream>
 #include <string>
 
+#include <osquery/logger.h>
+
 #include <boost/iostreams/filter/gzip.hpp>
 #include <boost/iostreams/filtering_streambuf.hpp>
 
@@ -27,32 +29,36 @@
 
 #include "santa.h"
 
-const std::string LOG_PATH = "/var/db/santa/santa.log";
-const std::string PREFACE = "santad: ";
+const std::string kSantaLogPath = "/var/db/santa/santa.log";
+const std::string kLogEntryPreface = "santad: ";
 
-const std::string RULES_PATH = "/var/db/santa/rules.db";
-const std::string TEMP_RULES_PATH = "/tmp/rules.db";
+const std::string kSantaDatabasePath = "/var/db/santa/rules.db";
+const std::string kTemporaryDatabasePath = "/tmp/rules.db";
 
 std::list<std::string> archived_lines;
 unsigned int next_oldest_archive = 0;
 
 void extractValues(const std::string& line,
                    std::map<std::string, std::string>& values) {
+  values.clear();
+
   // extract timestamp
   size_t timestamp_start = line.find("[");
   size_t timestamp_end = line.find("]");
+
   if (timestamp_start != std::string::npos &&
       timestamp_end != std::string::npos && timestamp_start != timestamp_end) {
     values["timestamp"] =
         line.substr(timestamp_start + 1, timestamp_end - timestamp_start - 1);
   }
 
-  // extract key=value pairs after the preface
-  size_t key_pos = line.find(PREFACE);
+  // extract key=value pairs after the kLogEntryPreface
+  size_t key_pos = line.find(kLogEntryPreface);
   if (key_pos == std::string::npos) {
     return;
   }
-  key_pos += PREFACE.length();
+
+  key_pos += kLogEntryPreface.length();
   size_t key_end, val_pos, val_end;
   while ((key_end = line.find('=', key_pos)) != std::string::npos) {
     if ((val_pos = line.find_first_not_of("=", key_end)) == std::string::npos) {
@@ -74,14 +80,18 @@ void scrapeStream(std::istream& incoming,
                   bool save_to_archive = false) {
   std::string line;
   while (std::getline(incoming, line)) {
-    // explicitly filter to only include DENY events
-    if (line.find("decision=DENY") == std::string::npos) {
+    // explicitly filter to only include EXEC actions
+    if (line.find("action=EXEC") == std::string::npos) {
       continue;
     }
 
     std::map<std::string, std::string> values;
     extractValues(line, values);
-    response.push_back({values["timestamp"], values["path"], values["reason"], values["sha256"]});
+
+    response.push_back({values["timestamp"],
+                        values["path"],
+                        values["reason"],
+                        values["sha256"]});
 
     if (save_to_archive) {
       archived_lines.push_back(line);
@@ -90,14 +100,15 @@ void scrapeStream(std::istream& incoming,
 }
 
 void scrapeCurrentLog(LogEntries& response) {
+  response.clear();
+
   std::ifstream log_file;
-  log_file.open(LOG_PATH);
+  log_file.open(kSantaLogPath);
   if (!log_file.is_open()) {
     return;
   }
 
   scrapeStream(log_file, response);
-
   log_file.close();
 }
 
@@ -106,11 +117,12 @@ bool scrapeCompressedSantaLog(std::string file_path, LogEntries& response) {
   if (!log_file.is_open()) {
     return false;
   }
+
   boost::iostreams::filtering_streambuf<boost::iostreams::input> in;
   in.push(boost::iostreams::gzip_decompressor());
   in.push(log_file);
-  std::istream incoming(&in);
 
+  std::istream incoming(&in);
   scrapeStream(incoming, response, true);
 
   log_file.close();
@@ -119,7 +131,7 @@ bool scrapeCompressedSantaLog(std::string file_path, LogEntries& response) {
 
 bool newArchiveFileExists() {
   std::stringstream strstr;
-  strstr << LOG_PATH << "." << next_oldest_archive << ".gz";
+  strstr << kSantaLogPath << "." << next_oldest_archive << ".gz";
   std::ifstream file(strstr.str(), std::ios_base::in | std::ios_base::binary);
   return file.is_open();
 }
@@ -130,28 +142,40 @@ void processArchivedLines(LogEntries& response) {
        ++iter) {
     std::map<std::string, std::string> values;
     extractValues(*iter, values);
-    response.push_back({values["timestamp"], values["path"], values["reason"], values["sha256"]});
+    response.push_back({values["timestamp"],
+                        values["path"],
+                        values["reason"],
+                        values["sha256"]});
   }
 }
 
-void scrapeSantaLog(LogEntries& response) {
-  scrapeCurrentLog(response);
+bool scrapeSantaLog(LogEntries& response) {
+  try {
+    scrapeCurrentLog(response);
 
-  // if there are no new archived files, just process our stash
-  if (!newArchiveFileExists()) {
-    processArchivedLines(response);
-    return;
-  }
-
-  // rolling archive files--clear the stored archive and reprocess them all
-  archived_lines.clear();
-  for (unsigned int i = 0;; ++i) {
-    next_oldest_archive = i;
-    std::stringstream strstr;
-    strstr << LOG_PATH << "." << i << ".gz";
-    if (!scrapeCompressedSantaLog(strstr.str(), response)) {
-      break;
+    // if there are no new archived files, just process our stash
+    if (!newArchiveFileExists()) {
+      processArchivedLines(response);
+      return true;
     }
+
+    // rolling archive files--clear the stored archive and reprocess them all
+    archived_lines.clear();
+    for (unsigned int i = 0;; ++i) {
+      next_oldest_archive = i;
+
+      std::stringstream strstr;
+      strstr << kSantaLogPath << "." << i << ".gz";
+      if (!scrapeCompressedSantaLog(strstr.str(), response)) {
+        break;
+      }
+    }
+
+    return true;
+
+  } catch (const std::exception& e) {
+    VLOG(1) << "Failed to read the Santa log files: " << e.what();
+    return false;
   }
 }
 
@@ -159,53 +183,129 @@ static int rulesCallback(void* context,
                          int argc,
                          char** argv,
                          char** azColName) {
+  // clang-format off
+
+  // Expected argc/argv format:
+  //     shasum,           state,        type
+  //     shasum, white/blacklist, binary/cert
+
+  // clang-format on
+
   RuleEntries* rules = static_cast<RuleEntries*>(context);
-  if (argc == 3) {
-    // shasum, state, type
-    // shasum, white/blacklist, binary/cert
-    rules->push_back({std::string(argv[0]),
-                      argv[1][0] == '1' ? "whitelist" : "blacklist",
-                      argv[2][0] == '1' ? "binary" : "certificate"});
+  if (argc != 3) {
+    return 0;
   }
+
+  RuleEntry new_rule;
+  new_rule.shasum = argv[0];
+  new_rule.state = (argv[1][0] == '1') ? RuleEntry::State::Whitelist
+                                       : RuleEntry::State::Blacklist;
+
+  new_rule.type = (argv[2][0] == '1') ? RuleEntry::Type::Binary
+                                      : RuleEntry::Type::Certificate;
+
+  rules->push_back(std::move(new_rule));
   return 0;
 }
 
-void collectSantaRules(RuleEntries& response) {
+bool collectSantaRules(RuleEntries& response) {
+  response.clear();
+
   // make a copy of the rules db (santa keeps the db locked)
-  std::ifstream src(RULES_PATH, std::ios_base::binary);
+  std::ifstream src(kSantaDatabasePath, std::ios_base::binary);
   if (!src.is_open()) {
-    response.push_back({"error", "failed to open rules.db", ""});
-    return;
+    VLOG(1) << "Failed to access the Santa rule database";
+    return false;
   }
-  std::ofstream dst(TEMP_RULES_PATH,
+
+  std::ofstream dst(kTemporaryDatabasePath,
                     std::ios_base::binary | std::ios_base::trunc);
+
   if (!dst.is_open()) {
-    response.push_back({"error", "failed to open /tmp/rules.db", ""});
-    return;
+    VLOG(1) << "Failed to duplicate the Santa rule database";
+    return false;
   }
 
   dst << src.rdbuf();
   src.close();
   dst.close();
 
+  // Open the database copy and enumerate the rules
   sqlite3* db;
-  int rc = sqlite3_open(TEMP_RULES_PATH.c_str(), &db);
-  if (0 != rc) {
-    // failed to open the database
-    response.push_back({"error", "failed to open database", ""});
-    return;
+  int rc = sqlite3_open(kTemporaryDatabasePath.c_str(), &db);
+  if (SQLITE_OK != rc) {
+    VLOG(1) << "Failed to read Santa rule database";
+    return false;
   }
 
-  char* zErrorMessage = 0;
+  char* sqlite_error_message = nullptr;
   rc = sqlite3_exec(db,
                     "SELECT shasum, state, type FROM rules;",
                     rulesCallback,
                     &response,
-                    &zErrorMessage);
+                    &sqlite_error_message);
+
   if (rc != SQLITE_OK) {
-    sqlite3_free(zErrorMessage);
-    response.push_back({"error", "failed to execute query", ""});
+    VLOG(1) << "Failed to query the Santa rule database: "
+            << (sqlite_error_message != nullptr ? sqlite_error_message : "");
+  }
+
+  if (sqlite_error_message != nullptr) {
+    sqlite3_free(sqlite_error_message);
   }
 
   sqlite3_close(db);
+  return (rc == SQLITE_OK);
+}
+
+const char* getRuleTypeName(RuleEntry::Type type) {
+  switch (type) {
+  case RuleEntry::Type::Binary:
+    return "binary";
+
+  case RuleEntry::Type::Certificate:
+    return "certificate";
+
+  case RuleEntry::Type::Unknown:
+  default:
+    return "unknown";
+  }
+}
+
+const char* getRuleStateName(RuleEntry::State state) {
+  switch (state) {
+  case RuleEntry::State::Whitelist:
+    return "whitelist";
+
+  case RuleEntry::State::Blacklist:
+    return "blacklist";
+
+  case RuleEntry::State::Unknown:
+  default:
+    return "unknown";
+  }
+}
+
+RuleEntry::Type getTypeFromRuleName(const char* name) {
+  std::string type_name(name);
+
+  if (type_name == "certificate") {
+    return RuleEntry::Type::Certificate;
+  } else if (type_name == "binary") {
+    return RuleEntry::Type::Binary;
+  } else {
+    return RuleEntry::Type::Unknown;
+  }
+}
+
+RuleEntry::State getStateFromRuleName(const char* name) {
+  std::string state_name(name);
+
+  if (state_name == "blacklist") {
+    return RuleEntry::State::Blacklist;
+  } else if (state_name == "whitelist") {
+    return RuleEntry::State::Whitelist;
+  } else {
+    return RuleEntry::State::Unknown;
+  }
 }
---
+++ santa/src/santarulestable.cpp
@@ -0,0 +1,364 @@
+/*
+ * Copyright (c) 2018 Trail of Bits, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <atomic>
+#include <mutex>
+
+#include <osquery/core/conversions.h>
+#include <osquery/logger.h>
+
+#include "santa.h"
+#include "santarulestable.h"
+#include "utils.h"
+
+REGISTER_EXTERNAL(SantaRulesTablePlugin, "table", "santa_rules");
+
+namespace {
+const std::string kSantactlPath = "/usr/local/bin/santactl";
+const std::string kMandatoryRuleDeletionError =
+    "Failed to modify rules: A required rule was requested to be deleted";
+
+using RowID = std::uint32_t;
+
+RowID generateRowID() {
+  static std::atomic_uint32_t generator(0U);
+  return generator++;
+}
+
+std::string generatePrimaryKey(const std::string& shasum, bool is_certificate) {
+  return shasum + "_" + (is_certificate ? "certificate" : "binary");
+}
+
+std::string generatePrimaryKey(const RuleEntry& rule) {
+  auto is_certificate = (rule.type == RuleEntry::Type::Certificate);
+  return generatePrimaryKey(rule.shasum, is_certificate);
+}
+} // namespace
+
+struct SantaRulesTablePlugin::PrivateData final {
+  std::mutex mutex;
+
+  std::unordered_map<RowID, std::string> rowid_to_pkey;
+  std::unordered_map<std::string, RuleEntry> rule_list;
+};
+
+osquery::Status SantaRulesTablePlugin::GetRowData(
+    osquery::Row& row, const std::string& json_value_array) {
+  row.clear();
+
+  rapidjson::Document document;
+  document.Parse(json_value_array);
+  if (document.HasParseError() || !document.IsArray()) {
+    return osquery::Status(1, "Invalid json received by osquery");
+  }
+
+  if (document.Size() != 3U) {
+    return osquery::Status(1, "Wrong column count");
+  }
+
+  if (document[0].IsNull()) {
+    return osquery::Status(1, "Missing 'shasum' value");
+  }
+
+  if (document[1].IsNull()) {
+    return osquery::Status(1, "Missing 'state' value");
+  }
+
+  if (document[2].IsNull()) {
+    return osquery::Status(1, "Missing 'type' value");
+  }
+
+  row["shasum"] = document[0].GetString();
+  if (row["shasum"].length() != 64 ||
+      std::string::npos !=
+          row["shasum"].find_first_not_of("0123456789abcdef")) {
+    return osquery::Status(1, "Invalid 'shasum' value");
+  }
+
+  row["state"] = document[1].GetString();
+  if (row["state"] != "whitelist" && row["state"] != "blacklist") {
+    return osquery::Status(1, "Invalid 'state' value");
+  }
+
+  row["type"] = document[2].GetString();
+  if (row["type"] != "binary" && row["type"] != "certificate") {
+    return osquery::Status(1, "Invalid 'type' value");
+  }
+
+  return osquery::Status(0, "OK");
+}
+
+SantaRulesTablePlugin::SantaRulesTablePlugin() : d(new PrivateData) {}
+
+SantaRulesTablePlugin::~SantaRulesTablePlugin() {}
+
+osquery::TableColumns SantaRulesTablePlugin::columns() const {
+  // clang-format off
+  return {
+      std::make_tuple("shasum",
+                      osquery::TEXT_TYPE,
+                      osquery::ColumnOptions::DEFAULT),
+
+      std::make_tuple("state",
+                      osquery::TEXT_TYPE,
+                      osquery::ColumnOptions::DEFAULT),
+
+      std::make_tuple("type",
+                      osquery::TEXT_TYPE,
+                      osquery::ColumnOptions::DEFAULT)
+  };
+  // clang-format on
+}
+
+osquery::QueryData SantaRulesTablePlugin::generate(
+    osquery::QueryContext& request) {
+  std::unordered_map<RowID, std::string> rowid_to_pkey;
+  std::unordered_map<std::string, RuleEntry> rule_list;
+
+  {
+    std::lock_guard<std::mutex> lock(d->mutex);
+
+    auto status = updateRules();
+    if (!status.ok()) {
+      VLOG(1) << status.getMessage();
+      return {{std::make_pair("status", "failure")}};
+    }
+
+    rowid_to_pkey = d->rowid_to_pkey;
+    rule_list = d->rule_list;
+  }
+
+  osquery::QueryData result;
+
+  for (const auto& rowid_pkey_pair : rowid_to_pkey) {
+    const auto& rowid = rowid_pkey_pair.first;
+    const auto& pkey = rowid_pkey_pair.second;
+
+    auto rule_it = rule_list.find(pkey);
+    if (rule_it == rule_list.end()) {
+      VLOG(1) << "RowID -> Primary key mismatch error in santa_rules table";
+      continue;
+    }
+
+    const auto& rule = rule_it->second;
+
+    osquery::Row row;
+    row["rowid"] = std::to_string(rowid);
+    row["shasum"] = rule.shasum;
+    row["state"] = getRuleStateName(rule.state);
+    row["type"] = getRuleTypeName(rule.type);
+
+    result.push_back(std::move(row));
+  }
+
+  return result;
+}
+
+osquery::QueryData SantaRulesTablePlugin::insert(
+    osquery::QueryContext& context, const osquery::PluginRequest& request) {
+  static_cast<void>(context);
+  std::lock_guard<std::mutex> lock(d->mutex);
+
+  osquery::Row row;
+  auto status = GetRowData(row, request.at("json_value_array"));
+  if (!status.ok()) {
+    VLOG(1) << status.getMessage();
+    return {{std::make_pair("status", "failure")}};
+  }
+
+  bool whitelist = row["state"] == "whitelist";
+  bool certificate = row["type"] == "certificate";
+  const auto& shasum = row.at("shasum");
+
+  std::vector<std::string> santactl_args = {
+      "rule", whitelist ? "--whitelist" : "--blacklist", "--sha256", shasum};
+
+  if (certificate) {
+    santactl_args.push_back("--certificate");
+  }
+
+  // The command always succeeds, even if the rule already exists; this is
+  // an issue for us because we have to return a valid rowid (and we can't
+  // duplicate entries)
+  ProcessOutput santactl_output;
+  if (!ExecuteProcess(santactl_output, kSantactlPath, santactl_args) ||
+      santactl_output.exit_code != 0) {
+    VLOG(1) << "Failed to add the rule";
+    return {{std::make_pair("status", "failure")}};
+  }
+
+  // Enumerate the rules and search for the one we just added
+  status = updateRules();
+  if (!status.ok()) {
+    VLOG(1) << status.getMessage();
+    return {{std::make_pair("status", "failure")}};
+  }
+
+  bool rule_found = false;
+  RowID row_id = 0U;
+  auto primary_key = generatePrimaryKey(shasum, certificate);
+
+  for (const auto& rowid_pkey_pair : d->rowid_to_pkey) {
+    const auto& rowid = rowid_pkey_pair.first;
+    const auto& pkey = rowid_pkey_pair.second;
+
+    if (primary_key != pkey) {
+      continue;
+    }
+
+    auto rule_it = d->rule_list.find(primary_key);
+    if (rule_it == d->rule_list.end()) {
+      VLOG(1) << "RowID -> Primary Key mismatch in the santa_rules table";
+      continue;
+    }
+
+    const auto& rule = rule_it->second;
+    if (rule.type != getTypeFromRuleName(row["type"].data())) {
+      continue;
+    }
+
+    if (rule.state != getStateFromRuleName(row["state"].data())) {
+      continue;
+    }
+
+    row_id = rowid;
+    rule_found = true;
+
+    break;
+  }
+
+  if (!rule_found) {
+    return {{std::make_pair("status", "failure")}};
+  }
+
+  osquery::Row result;
+  result["id"] = std::to_string(row_id);
+  result["status"] = "success";
+  return {result};
+}
+
+osquery::QueryData SantaRulesTablePlugin::delete_(
+    osquery::QueryContext& context, const osquery::PluginRequest& request) {
+  static_cast<void>(context);
+  std::lock_guard<std::mutex> lock(d->mutex);
+
+  RowID rowid;
+
+  {
+    unsigned long long temp;
+    auto status = osquery::safeStrtoull(request.at("id"), 10, temp);
+    if (!status.ok()) {
+      return {{std::make_pair("status", "failure")}};
+    }
+
+    rowid = static_cast<RowID>(temp);
+  }
+
+  auto pkey_it = d->rowid_to_pkey.find(rowid);
+  if (pkey_it == d->rowid_to_pkey.end()) {
+    return {{std::make_pair("status", "failure")}};
+  }
+
+  const auto& pkey = pkey_it->second;
+  auto rule_it = d->rule_list.find(pkey);
+  if (rule_it == d->rule_list.end()) {
+    VLOG(1) << "RowID -> Primary Key mismatch in the santa_rules table";
+    return {{std::make_pair("status", "failure")}};
+  }
+
+  const auto& rule = rule_it->second;
+  std::vector<std::string> santactl_args = {
+      "rule", "--remove", "--sha256", rule.shasum};
+
+  if (rule.type == RuleEntry::Type::Certificate) {
+    santactl_args.push_back("--certificate");
+  }
+
+  // The command always succeeds, even if the rule does not exists
+  ProcessOutput santactl_output;
+  if (!ExecuteProcess(santactl_output, kSantactlPath, santactl_args) ||
+      santactl_output.exit_code != 0) {
+    // Some rules can't be removed
+    if (santactl_output.std_output.find(kMandatoryRuleDeletionError) == 0) {
+      VLOG(1) << "Rule "
+              << rule.shasum + "/" + getRuleTypeName(rule.type) +
+                     " is mandatory and can't be removed";
+    } else {
+      VLOG(1) << "Failed to remove the rule";
+    }
+
+    return {{std::make_pair("status", "failure")}};
+  }
+
+  auto status = updateRules();
+  if (!status.ok()) {
+    VLOG(1) << status.getMessage();
+    return {{std::make_pair("status", "failure")}};
+  }
+
+  return {{std::make_pair("status", "success")}};
+}
+
+osquery::QueryData SantaRulesTablePlugin::update(
+    osquery::QueryContext& context, const osquery::PluginRequest& request) {
+  static_cast<void>(context);
+  static_cast<void>(request);
+
+  VLOG(1) << "UPDATE statements are not supported on the santa_rules table";
+  return {{std::make_pair("status", "failure")}};
+}
+
+osquery::Status SantaRulesTablePlugin::updateRules() {
+  RuleEntries new_rule_list;
+  if (!collectSantaRules(new_rule_list)) {
+    return osquery::Status(1, "Failed to enumerate the Santa rules");
+  }
+
+  auto old_rowid_mappings = std::move(d->rowid_to_pkey);
+  d->rowid_to_pkey.clear();
+
+  d->rule_list.clear();
+
+  for (const auto& new_rule : new_rule_list) {
+    auto primary_key = generatePrimaryKey(new_rule);
+    d->rule_list.insert({primary_key, new_rule});
+
+    RowID rowid;
+
+    {
+      // clang-format off
+      auto it = std::find_if(
+        old_rowid_mappings.begin(),
+        old_rowid_mappings.end(),
+
+        [primary_key](const std::pair<RowID, std::string> &pkey_rowid_pair) -> bool {
+          return (primary_key == pkey_rowid_pair.second);
+        }
+      );
+      // clang-format on
+
+      if (it == old_rowid_mappings.end()) {
+        rowid = generateRowID();
+      } else {
+        rowid = it->first;
+      }
+    }
+
+    d->rowid_to_pkey.insert({rowid, primary_key});
+  }
+
+  return osquery::Status(0);
+}
